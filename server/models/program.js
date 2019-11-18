b62e853e92fb69178cf3b63ae0602f123e5fbd94
denis212 <shotokhaw213@gmail.com>
2019-11-18 14:32:59 +0700

    programs component created

--- server/models/program.js
+++ server/models/program.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const programSchema = new Schema({
-  name: {
+  title: {
     type: String,
     required: true,
     minlength: 6,
